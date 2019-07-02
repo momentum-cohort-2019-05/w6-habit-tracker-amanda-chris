292d50233d959dc0b8541491aa7f2a95a0cd4bb4
Amanda Phillipson <amanda.minton@gmail.com>
2019-07-02 17:09:40 -0400

    trying to both migrate

--- core/models.py
+++ core/models.py
@@ -9,21 +9,19 @@ class Habit(models.Model):
         help_text="What habit do you want to track?",
         null=True
     )
-    target = models.TextField(
-        max_length=200,
-        help_text="How many things you wanna do?",
+    target = models.IntegerField(
+        help_text="How many is your target?",
         null=True
     )
-    date = models.DateField(auto_now_add=True, null=True)
+    date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
 
 class DailyRecord(models.Model):
     entry = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='habits')
-    text = models.TextField(
-        max_length=200,
-        help_text="How many things you wanna do?",
+    achieved = models.IntegerField(
+        help_text="How many X did you today?",
         null=True)
     date = models.DateField(default=date.today)
     
